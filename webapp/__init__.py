ad9642ffb7469702fbe6f923c02f42569fe860a5
Roman <dizels@gmail.com>
2020-06-16 20:32:00 +0300

    авторизация

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,12 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for
+from flask_login import LoginManager, login_user, logout_user
 
 from webapp.model import db, News
 from webapp.weather import weather_by_city
+from webapp.forms import LoginForm
+
 
 def create_app():
     app = Flask(__name__)
     app.config.from_pyfile("config.py")
     db.init_app(app)
+
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+    login_manager.login_view = "login"
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(user_id)
 
     @app.route("/")
     def index():
@@ -14,4 +25,29 @@ def create_app():
         weather = weather_by_city(app.config["WEATHER_DEFAULT_CITY"])
         news_list = News.query.order_by(News.published.desc()).all()
         return render_template("index.html", page_title=title, weather=weather, news_list=news_list)
+
+    @app.route("/login")
+    def login():
+        title = "Авторизация"
+        login_form = LoginForm()
+        return render_template("login.html", page_title=title, form=login_form)
+
+    @app.route("/process-login", method=["POST"])
+    def process_login():
+        form = LoginForm()
+        if form.validate_on_submit():
+            user = User.query.filter(User.username == form.username.data).first()
+            if user and user.check_password(form.password.data):
+                login_user(user)
+                flash("Вы успешно авторизировались")
+                return redirect(url_for("index"))
+        flash("Некорректные имя или пароль")
+        return redirect(url_for("login"))
+
+    @app.route("/logout")
+    def logout():
+        logout_user()
+        flash("Вы вышли из аккаунта")
+        return redirect(url_for("index"))
+
     return app
